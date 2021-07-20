1971f379ea54629282df6d19537d096967296376
abdulazeez-dolapo <abdulrafiua10@gmail.com>
2021-07-20 12:02:15 +0100

    create token

--- migrations/2_deploy.js
+++ migrations/2_deploy.js
@@ -1,14 +1,15 @@
-const Token = artifacts.require("Token");
-const dBank = artifacts.require("dBank");
+const Token = artifacts.require("Token")
+const dBank = artifacts.require("dBank")
 
-module.exports = async function(deployer) {
+module.exports = async function (deployer) {
 	//deploy Token
+	await deployer.deploy(Token)
 
 	//assign token into variable to get it's address
-	
+
 	//pass token address for dBank contract(for future minting)
 
 	//assign dBank contract into variable to get it's address
 
 	//change token's owner/minter from deployer to dBank
-};
+}
--- src/contracts/Token.sol
+++ src/contracts/Token.sol
@@ -5,17 +5,29 @@ import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 contract Token is ERC20 {
   //add minter variable
+  address public minter;
 
   //add minter changed event
+  event MinterChanged(address indexed from, address to);
 
-  constructor() public payable ERC20("Name", "Symbol") {
+  constructor() public payable ERC20("AZ meme currency", "AZM") {
     //asign initial minter
+    minter = msg.sender;
   }
 
   //Add pass minter role function
+  function passMinterRole(address dBank) public returns (bool) {
+    //check if the sender is the minter
+    require(msg.sender == minter, 'Error: Only owner can change pass minter role');
+    //set the new minter
+    minter = dBank;
+    //emit the minter changed event
+    emit MinterChanged(msg.sender, dBank);
+    return true;
+  }
 
   function mint(address account, uint256 amount) public {
-    //check if msg.sender have minter role
+    require(msg.sender == minter, 'Error: Only minter can mint tokens.');
 		_mint(account, amount);
 	}
 }
